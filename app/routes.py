804049b23207284227d639e9e0e222674faa2385
Matt Day <mattday@matts-mac-mini.lan>
2023-01-19 16:40:25 -0500

    third commit

--- app/routes.py
+++ app/routes.py
@@ -3,22 +3,30 @@ from flask import render_template, request, redirect, url_for
 from .forms import UserCreationForm
 import requests as r
 
-@app.route('/index', methods=['GET', 'POST'])
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def findpokemon():
     poke = UserCreationForm()
-    url = f'https://pokeapi.co/api/v2/pokemon/pikachu'
-    response = r.get(url)
-    my_dict = response.json()
-    if response.ok:
-        # print(my_dict)
-        pokemon = [my_dict['name'], my_dict['abilities'][0]['ability']['name'], my_dict['base_experience'], my_dict['sprites']['front_shiny'], my_dict['stats'][1]['base_stat'], my_dict['stats'][0]['base_stat'], my_dict['stats'][2]['base_stat']]
-        image = pokemon[3]
-        info = (f'Name:  {pokemon[0]}\n  Ability:  {pokemon[1]}  \nBase Experience:  {pokemon[2]}  \nAttack Base Stat:  {pokemon[4]}  \nHP Base Stat:  {pokemon[5]}  \nDefense Base Stat:  {pokemon[6]}\n')
-    else:
-        return "The pokemon you're looking for does not exist."
+    if request.method == 'POST':
+        url = f'https://pokeapi.co/api/v2/pokemon/{poke.choose.data}'
+        response = r.get(url)
+        if response.ok:
+            # print(my_dict)
+            my_dict = response.json()
+            pokemon_dict = {}
+            pokemon_dict["Name"] = my_dict["name"]
+            pokemon_dict["Ability"] = my_dict["abilities"][0]["ability"]["name"]
+            pokemon_dict["Base XP"] = my_dict["base_experience"]
+            pokemon_dict["Front Shiny"] = my_dict["sprites"]["front_shiny"]
+            pokemon_dict["Base ATK"] = my_dict["stats"][1]["base_stat"]
+            pokemon_dict["Base HP"] = my_dict["stats"][0]["base_stat"]
+            pokemon_dict["Base DEF"] = my_dict["stats"][2]["base_stat"]
+        else:
+            return "The pokemon you're looking for does not exist."
 
-    return render_template('index.html', poke = poke, pokemon = pokemon, info = info, image=image)
+        return render_template('index.html', poke = poke, pokemon_dict = pokemon_dict)
+    
+    return render_template('index.html', poke = poke)
+
 
 
 
